64e698e9ea3dd98b97dd1538d8c9ac18a069a6fb
justinsitarz <justinsitarz+github@gmail.com>
2020-08-21 11:55:06 -0500

    fix integration export script and remove key

--- export_integrations.py
+++ export_integrations.py
@@ -4,7 +4,7 @@ import json
 import pandas as pd
 import sys
 
-api_key = '<your apu key>'
+api_key = '<your api key>'
 integration_data_csv = './integrationList.csv' # set this to your desired path or leave the same to generate csv in same directory
 api_headers = {'Content-Type': 'application/json','Authorization':'GenieKey ' + api_key}
 list_integration_url = "https://api.opsgenie.com/v2/integrations/" 
@@ -13,22 +13,17 @@ expand_params = {'expand': 'contact'}
 
 def build_integration_list(api_key, url, api_headers):
     integration_list_request = requests.get(url = list_integration_url, headers = api_headers)
-    integration_list = json.loads(integration_list_request.text) 
-    return integration_list
+    integration_json = json.loads(integration_list_request.text) 
+    integrations = []
+    
+    for integration in integration_json['data']:
+    	integrations.append(integration['name'])
 
-# def generate_csv(integrations):
-#     df = pd.DataFrame(integrations)
-#     df_transposed = df.transpose()
-#     df_transposed.to_csv(integration_data_csv, sep=',', encoding='utf-8')
+    return integrations
 
 def generate_csv(integrations):
-    print(integrations[0])
-    keys = integrations[0].keys()
-    with open(integration_data_csv, 'w', newline='')  as output_file:
-	    dict_writer = csv.DictWriter(output_file, keys)
-	    dict_writer.writeheader()
-	    dict_writer.writerows(integrations)
+    df = pd.DataFrame(integrations)
+    df.to_csv(integration_data_csv, sep=',', encoding='utf-8')
 
 integrations = build_integration_list(api_key, list_integration_url, api_headers)
 generate_csv(integrations)
-
